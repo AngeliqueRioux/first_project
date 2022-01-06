44b110ab370a8a3b903c117a234ee36b4a754ef6
AngéliqueRioux <allange002@gmail.com>
2022-01-06 10:28:06 +0100

    projet améliorer 4 pages sur 6

--- app.js
+++ app.js
@@ -4,14 +4,17 @@ let clickBurger = false
 function toggleMenu() {
     const burger = document.querySelector('.burger');
     const navbar = document.querySelector('.navbar');
-    const navul = document.querySelector('.nav-ul')
-    const title = document.getElementById('title')
-    
+    const navul = document.querySelector('.nav-ul');
+
     burger.addEventListener('click', () => {
         navbar.classList.toggle('active');
-        navul.classList.toggle('active')
+        navul.classList.toggle('active');    
     })
 }
 
 toggleMenu();
 
+function myFunction() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+}
